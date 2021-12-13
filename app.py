93faaf2cec29d9b3eb26bba357d649f144a3c130
aziyan99 <rajaazian08@gmail.com>
2021-12-13 17:03:07 +0700

    load model

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-from flask import Flask, render_template, request
-# import pickle
+from flask import Flask, render_template, request, jsonify
+import pickle
+import sklearn
 
 
 app = Flask(__name__)
 
 
+model = pickle.load(open('./models/model.regr', 'rb'))
+
+
 @app.route('/')
 def index():
-    return render_template("index.html", hasil=0)
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    pass
+    pintu = request.form['pintu']
+    bulan = request.form['bulan']
+    model_res = model.predict([[int(pintu), int(bulan)]])
+    res = {
+        'status': 'success',
+        'prediction': model_res[0][0]
+    }
+    return jsonify(res)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
